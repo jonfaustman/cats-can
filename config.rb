115cd5255b643a37c7e052337e65bfb96d85f0c7
Jon Faustman <faustman@k-state.edu>
2014-06-17 16:49:14 -0500

    Initial styles.

--- config.rb
+++ config.rb
@@ -1,4 +1,5 @@
 require 'compass/import-once/activate'
+require 'color-schemer'
 # Require any additional compass plugins here.
 
 # Set this to the root of your project when deployed:
@@ -9,12 +10,12 @@ images_dir = "static/img"
 javascripts_dir = "static/js"
 
 # You can select your preferred output style here (can be overridden via the command line):
-# output_style = :expanded or :nested or :compact or :compressed
+output_style = :expanded
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
-# relative_assets = true
+relative_assets = true
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
-# line_comments = false
+line_comments = false
 
 preferred_syntax = :sass
